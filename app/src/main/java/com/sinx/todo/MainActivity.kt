f486134aeb581c898a2adc084bb2eaf4e88e627a
AlexeyGvozdev <gvozdev.alexey95@mail.ru>
2021-02-05 20:21:57 +0300

    init commit

---
+++ app/src/main/java/com/sinx/todo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.sinx.todo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
