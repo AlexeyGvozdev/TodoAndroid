4a21d2553983fdfec9c06222105e04ff33bc496d
AlexeyGvozdev <gvozdev.alexey95@mail.ru>
2021-02-12 22:51:13 +0300

    add binding and viewmodel to AddTaskBottomSheet

--- app/src/main/java/com/sinx/todo/ui/add/task/AddTaskBottomSheet.kt
+++ app/src/main/java/com/sinx/todo/ui/add/task/AddTaskBottomSheet.kt
@@ -4,10 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.sinx.todo.R
+import com.sinx.todo.core.bottomSheetViewBinding
+import com.sinx.todo.databinding.FragmentAddTaskBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filterNotNull
 
 class AddTaskBottomSheet : BottomSheetDialogFragment() {
+
+    private val binding: FragmentAddTaskBinding by bottomSheetViewBinding(FragmentAddTaskBinding::bind)
+    private val viewModel by viewModels<AddTaskViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +25,40 @@ class AddTaskBottomSheet : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_add_task, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.close.setOnClickListener {
+            viewModel.dispatch(AddTaskMsg.OnClosePressed)
+        }
+        binding.create.setOnClickListener {
+            viewModel.dispatch(AddTaskMsg.OnCreatePressed(binding.taskName.text.toString()))
+        }
+        lifecycleScope.launchWhenCreated {
+            viewModel.viewAction().filterNotNull().collect(::doAction)
+        }
+        lifecycleScope.launchWhenStarted {
+            viewModel.viewStates().filterNotNull().collect(::render)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.AppBottomSheetDialogTheme)
+    }
+
+    private fun render(state: AddTaskViewState) {
+
+    }
+
+    private fun doAction(action: AddTaskAction) {
+        when (action) {
+            AddTaskAction.CloseDialog -> dismiss()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
     }
 }
---
+++ app/src/main/java/com/sinx/todo/ui/add/task/AddTaskViewModel.kt
@@ -0,0 +1,46 @@
+package com.sinx.todo.ui.add.task
+
+import com.sinx.todo.base.BaseViewModel
+import com.sinx.todo.core.Update
+import com.sinx.todo.core.ViewState
+
+class AddTaskViewModel :
+    BaseViewModel<AddTaskModel, AddTaskViewState, AddTaskAction, AddTaskMsg>() {
+
+    private var model: AddTaskModel = AddTaskModel()
+
+    override fun dispatch(msg: AddTaskMsg) {
+        model = update(model, msg)
+        viewState = view(model)
+    }
+
+    private val update: Update<AddTaskModel, AddTaskMsg> = { model, msg ->
+        when (msg) {
+            is AddTaskMsg.OnCreatePressed -> {
+                viewAction = AddTaskAction.CloseDialog
+                model
+            }
+            AddTaskMsg.OnClosePressed -> {
+                viewAction = AddTaskAction.CloseDialog
+                model
+            }
+        }
+    }
+
+    private val view: ViewState<AddTaskModel, AddTaskViewState> = { mdoel ->
+        AddTaskViewState()
+    }
+}
+
+class AddTaskModel()
+
+class AddTaskViewState()
+
+sealed class AddTaskMsg() {
+    class OnCreatePressed(taskName: String) : AddTaskMsg()
+    object OnClosePressed : AddTaskMsg()
+}
+
+sealed class AddTaskAction() {
+    object CloseDialog : AddTaskAction()
+}
